3892cb7749443a4265614afe0cc386a87a010666
Khuram Niaz <knshaikh@gmail.com>
2023-07-25 14:56:35 -0600

    Project initialization with boilerplate code

---
+++ tripplannerapi/src/main/java/com/example/tripplannerapi/TripplannerapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripplannerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripplannerapiApplication.class, args);
+	}
+
+}
---
+++ tripplannerapi/src/test/java/com/example/tripplannerapi/TripplannerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripplannerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
